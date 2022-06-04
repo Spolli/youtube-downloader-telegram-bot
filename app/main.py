6fe174c7548c1f3a6e5495d5f9af1494ce315974
Spolli <kebabnopiccante@gmail.com>
2022-06-04 02:14:54 +0200

    first commit

---
+++ app/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from __future__ import unicode_literals
+import youtube_dl
+import telebot
+import os
+
+TOKEN = os.environ("TOKEN") if "TOKEN" in os.environ else "5449367848:AAH3fCtqxv1Ryhp1zXI8C1jJEbMyvpff0rw"
+USERNAME = os.environ("USERNAME") if "USERNAME" in os.environ else "inutilita1@gmail.com"
+PASSWORD = os.environ("PASSWORD") if "PASSWORD" in os.environ else "9GS25!atQtM%6XTq"
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(regexp="^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$")
+def handle_message(message):
+    ydl_opts = {
+        'username': USERNAME,
+        'password': PASSWORD,
+        'format': 'bestaudio/best',
+        'outtmpl': './download/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+        'prefer_ffmpeg': True
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([message.text])
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+
+
